7b27cb69932581e5a020ea89e430c5305113d774
HalfMAI <halfloligo.h@gmail.com>
2018-10-04 13:48:31 +0800

    add static file gzip support

--- requestHandler.py
+++ requestHandler.py
@@ -5,6 +5,7 @@ import traceback
 import json
 import os
 import signal
+import zlib
 from mimetypes import types_map
 
 import utitls
@@ -16,10 +17,18 @@ from questInfo import checkIfInQuest, getQuestListStr, getQuestList_AddStarts, u
 
 class RequestHandler(BaseHTTPRequestHandler):
 
+    def gzip_encode(self, content):
+        gzip_compress = zlib.compressobj(9, zlib.DEFLATED, zlib.MAX_WBITS | 16)
+        data = gzip_compress.compress(content) + gzip_compress.flush()
+        return data
+
     def do_GET(self):
         request_path = self.path
         rc = 404
         rb = None
+        sh = {
+            'Content-Encoding': 'gzip'
+        }
         params = parse_qs(urlsplit(request_path).query)
 
         try:
@@ -29,12 +38,24 @@ class RequestHandler(BaseHTTPRequestHandler):
                     tmp_filePath = os.path.join(os.getcwd())
                     for v in request_path.split('/'):
                         tmp_filePath = os.path.join(tmp_filePath, v)
-                    print(tmp_filePath)
                     with open(tmp_filePath, 'r', encoding='utf-8') as f:
+                        lastMtime = self.date_time_string(os.fstat(f.fileno()).st_mtime)
+                        if self.headers.get('If-Modified-Since') == lastMtime:
+                            self.send_response(304)
+                            self.end_headers()
+                            return
+
                         self.send_response(200)
-                        self.send_header('Content-type', types_map[ext])
+                        sh['Content-type'] = types_map[ext]
+                        sh['Cache-Control'] = 'max-age=72000'
+                        fb = f.read()
+                        rb = self.gzip_encode(fb.encode('utf-8'))
+                        sh['Content-length'] = len(rb)
+                        sh['Last-Modified'] = lastMtime
+                        for key in sh:
+                            self.send_header(key, sh[key])
                         self.end_headers()
-                        self.wfile.write(f.read().encode('utf-8'))
+                        self.wfile.write(rb)
                 return
         except Exception:
             utitls.myLogger(traceback.format_exc())
@@ -42,50 +63,12 @@ class RequestHandler(BaseHTTPRequestHandler):
             self.end_headers()
             return
 
-        if request_path.startswith('/subscribe?'):
-            hub_challenge_list = params.get('hub.challenge', None)
-            if None != hub_challenge_list:
-                rc = 202
-                rb = hub_challenge_list[0]
+        if request_path.startswith('/get_manual_json'):
+            rc = 200
+            rb = json.dumps(utitls.manualJson())
         elif request_path.startswith('/questlist'):
             rc = 200
             rb = json.dumps(getQuestList_AddStarts())
-        elif request_path.startswith('/get_manual_json'):
-            rc = 200
-            rb = json.dumps(utitls.manualJson())
-        elif request_path.startswith('/addRestreamSrc'):
-            rc = 200
-            srcNote_list = params.get('srcNote', None)
-            srcLink_list = params.get('srcLink', None)
-            if srcNote_list and srcLink_list:
-                tmp_srcNote = srcNote_list[0].strip()
-                tmp_srcLink = srcLink_list[0].strip()
-                utitls.addManualSrc(tmp_srcNote, tmp_srcLink)
-            rb = json.dumps({"code": 0, "msg":"添加成功"})
-        elif request_path.startswith('/addRtmpDes'):
-            rc = 200
-            rtmpNote_list = params.get('rtmpNote', None)
-            rtmpLink_list = params.get('rtmpLink', None)
-            if rtmpNote_list and rtmpLink_list:
-                tmp_rtmpNote = rtmpNote_list[0].strip()
-                tmp_rtmpLink = rtmpLink_list[0].strip()
-                utitls.addManualDes(tmp_rtmpNote, tmp_rtmpLink)
-            rb = json.dumps({"code": 0, "msg":"添加成功"})
-        elif request_path.startswith('/kill_quest?'):
-            rc = 200
-            tmp_rtmpLink_list = params.get('rtmpLink', None)
-            if tmp_rtmpLink_list:
-                tmp_rtmpLink = tmp_rtmpLink_list[0].strip()
-                tmp_quest = _getObjWithRTMPLink(tmp_rtmpLink)
-                if tmp_quest != None:
-                    try:
-                        os.kill(tmp_quest.get('pid', None), signal.SIGKILL)
-                        rb = json.dumps({"code": 0, "msg": "操作成功"})
-                    except Exception:
-                        updateQuestWithIsDead(True, tmp_rtmpLink)
-                        rb = json.dumps({"code": 1, "msg": "正在重试，下次重试时会自动删除此任务"})
-                else:
-                    rb = json.dumps({"code": -1, "msg": "查找不到对应的任务：{}，操作失败!!".format(tmp_rtmpLink)})
         elif request_path.startswith('/live_restream?'):
             forwardLink_list = params.get('forwardLink', None)
             restreamRtmpLink_list = params.get('restreamRtmpLink', None)
@@ -120,15 +103,64 @@ class RequestHandler(BaseHTTPRequestHandler):
                 else:
                     rc = 200
                     rb = json.dumps({"code": -4, "msg": "RTMPLink格式错误!!! bilibili的RTMPLink格式是两串合起来。\nEXAMPLE：rtmp://XXXXXX.acg.tv/live-js/?streamname=live_XXXXXXX&key=XXXXXXXXXX"})
+        elif request_path.startswith('/kill_quest?'):
+            rc = 200
+            tmp_rtmpLink_list = params.get('rtmpLink', None)
+            if tmp_rtmpLink_list:
+                tmp_rtmpLink = tmp_rtmpLink_list[0].strip()
+                tmp_quest = _getObjWithRTMPLink(tmp_rtmpLink)
+                if tmp_quest != None:
+                    try:
+                        os.kill(tmp_quest.get('pid', None), signal.SIGKILL)
+                        rb = json.dumps({"code": 0, "msg": "操作成功"})
+                    except Exception:
+                        updateQuestWithIsDead(True, tmp_rtmpLink)
+                        rb = json.dumps({"code": 1, "msg": "当前任务正在重试连接中，下次任务重试连接时会自动删除此任务"})
+                else:
+                    rb = json.dumps({"code": -1, "msg": "查找不到对应的任务：{}，操作失败!!".format(tmp_rtmpLink)})
+        elif request_path.startswith('/addRestreamSrc?'):
+            rc = 200
+            srcNote_list = params.get('srcNote', None)
+            srcLink_list = params.get('srcLink', None)
+            if srcNote_list and srcLink_list:
+                tmp_srcNote = srcNote_list[0].strip()
+                tmp_srcLink = srcLink_list[0].strip()
+                utitls.addManualSrc(tmp_srcNote, tmp_srcLink)
+            rb = json.dumps({"code": 0, "msg":"添加成功"})
+        elif request_path.startswith('/addRtmpDes?'):
+            rc = 200
+            rtmpNote_list = params.get('rtmpNote', None)
+            rtmpLink_list = params.get('rtmpLink', None)
+            if rtmpNote_list and rtmpLink_list:
+                tmp_rtmpNote = rtmpNote_list[0].strip()
+                tmp_rtmpLink = rtmpLink_list[0].strip()
+                utitls.addManualDes(tmp_rtmpNote, tmp_rtmpLink)
+            rb = json.dumps({"code": 0, "msg":"添加成功"})
+        elif request_path.startswith('/subscribe?'):
+            hub_challenge_list = params.get('hub.challenge', None)
+            if None != hub_challenge_list:
+                rc = 202
+                rb = hub_challenge_list[0]
+
+
+#######
+        if rb:
+            rb = self.gzip_encode(rb.encode('utf-8'))
+            sh['Content-length'] = len(rb)
 
         self.send_response(rc)
-        self.send_header('Content-type', 'text/html; charset=utf-8')
+        for key in sh:
+            self.send_header(key, sh[key])
         self.end_headers()
+
         if None != rb:
             try:
-                self.wfile.write(rb.encode('utf-8'))
+                self.wfile.write(rb)
             except Exception:
                 print(traceback.format_exc())
+
+
+###############################################################
 
 
     def do_POST(self):
@@ -163,6 +195,7 @@ class RequestHandler(BaseHTTPRequestHandler):
                     tmp_feedTitle = root.find('dfns:title', ns).text
                     tmp_feedUpadatedTime = root.find('dfns:updated', ns).text
                     try:
+                        rc = 204
                         entry = root.findall('dfns:entry', ns)[0]       #maybe more than one?
                         tmp_entry_title = entry.find('dfns:title', ns).text
                         tmp_entry_videoId = entry.find('yt:videoId', ns).text
@@ -178,15 +211,9 @@ class RequestHandler(BaseHTTPRequestHandler):
                         #try to restream
                         Async_forwardToBilibili(tmp_entry_channelId, tmp_entry_link, tmp_entry_title, utitls.configJson().get('area_id'))
                     except Exception:
+                        rc = 404
                         utitls.myLogger(traceback.format_exc())
-                        self.send_response(rc)
-                        self.end_headers()
-                        return
-                    rc = 204
             else:
                 utitls.myLogger("verifySecert Failed with:" + secert)
-            self.send_response(rc)
-            self.end_headers()
-        else:
-            self.send_response(rc)
-            self.end_headers()
+        self.send_response(rc)
+        self.end_headers()
