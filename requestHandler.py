3eadcfeb879e9fd6fe8182491f1c78b741e1988d
HalfMAI <halfloligo.h@gmail.com>
2018-10-23 17:25:06 +0800

    add the twitter quest support from the Microsoft FLOW

--- requestHandler.py
+++ requestHandler.py
@@ -1,11 +1,13 @@
 from http.server import BaseHTTPRequestHandler
 from urllib.parse import urlsplit,parse_qs
 import xml.etree.ElementTree as ET
+import requests
+
 import traceback
 import json
 import os
-import signal
 import zlib
+import re
 from mimetypes import types_map
 
 import utitls
@@ -290,5 +292,26 @@ class RequestHandler(BaseHTTPRequestHandler):
                         utitls.myLogger(traceback.format_exc())
             else:
                 utitls.myLogger("verifySecert Failed with:" + secert)
+        elif '/tweet' == request_path:
+            # check the secert
+            secert = self.headers.get('Authorization', '')
+            if secert == utitls.configJson().get('subSecert', ''):
+                try:
+                    postDict = json.loads(post_data)
+                    tmp_acc = postDict.get('twitter_acc', "")
+                    tmp_body = postDict.get('twitter_body', "")
+                    rc = 200
+
+                    link_list = re.findall(r"(https://t.co/.+)", tmp_body)
+                    link_list = list(set(link_list))    # remove the duplicates items
+                    if len(link_list) > 0:
+                        l = link_list[0]     # I THINK they will just sent one link, so just use the first one
+                        redirect_url = requests.get(l)      # get the redirect url
+                        cur_sub = utitls.getSubWithKey('twitterId', tmp_acc)
+                        if cur_sub:
+                            tmp_area_id = cur_sub.get('area_id', '33')
+                            Async_forwardToBilibili(cur_sub, redirect_url, "THIS TITLE IS USENESS", tmp_area_id)
+                except Exception:
+                    utitls.myLogger(traceback.format_exc())
         self.send_response(rc)
         self.end_headers()
